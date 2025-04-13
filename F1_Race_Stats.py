6b71c4431a80f4e515ffeb9f1a51d17115907c0e
Jorge Gutierrez <jorgeegutierrez56615@gmail.com>
2025-04-12 21:53:08 -0700

    Saving progress

--- F1_Race_Stats.py
+++ F1_Race_Stats.py
@@ -1,5 +1,24 @@
+# Import all needed libraries
 from bs4 import BeautifulSoup
 import requests
 import re
+from datetime import datetime
+
+# Get the current year
+    # Note: we do this because the website updates the href every year with the current year
+now = datetime.now()
+current_year = now.year
+
+# Navigate to the Results page from the HomePage
+homepage_URL = f"https://www.formula1.com/"
+homepage = requests.get(homepage_URL).text
+homepage_doc = BeautifulSoup(homepage, "html.parser")
+
+results_URL = homepage_doc.find(href=f"https://www.formula1.com/en/results/{current_year}/races").get("href")
+results_page = requests.get(results_URL).text
+results_doc = BeautifulSoup(results_page, "html.parser")
+
+# Get all F1 Racing Season Years
+years_table = results_doc.find_all(attrs={'data-name': 'year'})
 
 
