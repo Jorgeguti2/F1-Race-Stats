7f8bbaf85a2c91781116f4f29aab804bd3f47352
Jorge Gutierrez <jorgeegutierrez56615@gmail.com>
2025-04-13 23:20:00 -0700

    Finished Scraping Driver Career Stats
    Retrieved all F1 driver's career stats. Need to optimize at some point however.

--- F1_Driver_Stats.py
+++ F1_Driver_Stats.py
@@ -16,4 +16,29 @@ drivers_doc = BeautifulSoup(drivers_page, "html.parser")
 # Get all F1 Racing Season Years
 years_table = drivers_doc.find_all(attrs={'data-name': 'year'})
 
-
+# Go year by year, driver by driver, grabbing all information (Grand Prix, Date, Car, Race Position, PTS)
+# Note: this gets all time career stats for each driver for each race and year
+    # Other Note: need to optimize :(((((
+for i in years_table:
+    year = i.text
+    url = f"https://www.formula1.com/en/results/{year}/drivers"
+    page = requests.get(url).text
+    doc = BeautifulSoup(page, "html.parser")
+    drivers = doc.find_all(attrs={'data-name': 'drivers'})
+    for driver in drivers:
+        driver_href = driver.find('a', href=True)
+        driver_URL = f"https://www.formula1.com{driver_href['href']}"
+        driver_page = requests.get(driver_URL).text
+        driver_doc = BeautifulSoup(driver_page, "html.parser")
+        tbody = driver_doc.find("tbody")
+        trs = tbody.contents
+        for i in trs:
+            tr = list(i)
+            grand_prix = tr[0].text
+            date = tr[1].text
+            car = (tr[2].text)[:-3]
+            pos = tr[3].text
+            pts = tr[4].text
+            print(driver.text, grand_prix, date, car, pos, pts)
+        print()
+    print()
